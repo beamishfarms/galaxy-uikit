28be531fb00f20f779eb56bb1be21d1a7dba9e9d
Gabriel Morales <gabbomorales1@gmail.com>
2021-02-27 17:09:15 -0600

    commit

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -1860,7 +1860,7 @@ var Icon$O = function (props) {
 var Logo = function (_a) {
     var isDark = _a.isDark, props = __rest(_a, ["isDark"]);
     return (React.createElement(Svg, __assign({ viewBox: "0 0 205 26" }, props),
-        React.createElement("image", { width: "205", height: "26", href: isDark ? 'images/galaxy/logowithtext.svg' : 'images/galaxy/logowithtext.svg' })));
+        React.createElement("image", { width: "130", height: "35", href: isDark ? 'images/galaxy/logowithtext.svg' : 'images/galaxy/logowithtext.svg' })));
 };
 
 var Icon$P = function (props) {
